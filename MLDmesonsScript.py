b25459dacfe92b9f4777947499f79c09f26cde5b
DanielMorris13 <danielmorris.sh@gmail.com>
2024-02-26 14:33:50 +0100

    name changing script

--- MLDmesonsScript.py
+++ MLDmesonsScript.py
@@ -15,4 +15,4 @@ def ML_DMesons ():
     plot_utils.plot_distr(hdl_mc, vars_to_draw, bins=1000, labels=leg_labels, log=True, density=True, figsize=(12, 7), alpha=0.3, grid=False)
     plt.subplots_adjust(left=0.06, bottom=0.06, right=0.99, top=0.96, hspace=0.55, wspace=0.55)
     plt.show()
-
+    
